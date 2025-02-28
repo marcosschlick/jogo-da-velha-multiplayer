2e3bfc4f0840dd045f5ceda010500ee280a537ad
Marcos Schlick <marcosschlick@gmail.com>
2025-02-27 23:54:13 -0300

    criar entidade Usuario

---
+++ src/entities/Usuario.java
@@ -0,0 +1,34 @@
+package entities;
+
+public class Usuario {
+    private String nome;
+    private char marcador;
+    private int vitorias;
+
+    public Usuario() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getMarcador() {
+        return marcador;
+    }
+
+    public void setMarcador(char marcador) {
+        this.marcador = marcador;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+}

772ecc9177c166cd40dc012986b3d52e66188b1b
Marcos Schlick <marcosschlick@gmail.com>
2025-02-28 01:18:32 -0300

    adicionar tela do resultado do jogo

---
+++ src/app/views/TelaResultado.java
@@ -0,0 +1,16 @@
+package app.views;
+
+import java.awt.CardLayout;
+import java.awt.Color;
+import javax.swing.JPanel;
+
+public class TelaResultado extends JPanel{
+
+    public TelaResultado(CardLayout cardLayout, JPanel cards) {
+
+        setLayout(null);
+
+        setBackground(new Color(180, 220, 255));
+
+    }
+}

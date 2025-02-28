fd6d3fbfc75102b4c94d4a4a1b04ca551bdfb02d
Marcos Schlick <marcosschlick@gmail.com>
2025-02-28 01:20:56 -0300

    adicionar cards das telas à main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import app.views.TelaInicial;
+import app.views.TelaJogo;
+import app.views.TelaResultado;
+import javax.swing.JFrame;
+import java.awt.CardLayout;
+import javax.swing.JPanel;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        JFrame frame = new JFrame("Jogo da velha");
+        frame.setSize(800, 900);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        CardLayout cardLayout = new CardLayout();
+        JPanel cards = new JPanel(cardLayout);
+
+        cards.add(new TelaInicial(cardLayout, cards), "TelaInicial");
+        cards.add(new TelaJogo(cardLayout, cards), "TelaJogo");
+        cards.add(new TelaResultado(cardLayout, cards), "TelaResultado");
+        frame.add(cards);
+
+        frame.setVisible(true);
     }
 }
